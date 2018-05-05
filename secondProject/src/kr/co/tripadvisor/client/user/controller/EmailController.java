b63048e11ef3878b31dbabc5107c41bbef17fdc7
hyeri <lilyantina@naver.com>
2018-05-06 01:18:57 +0900

    이메일 발송 계정을 gmail로 수정해보았습니다. 테스트 부탁드립니다.

--- secondProject/src/kr/co/tripadvisor/client/user/controller/EmailController.java
+++ secondProject/src/kr/co/tripadvisor/client/user/controller/EmailController.java
@@ -28,8 +28,8 @@ public class EmailController extends HttpServlet {
 			throws ServletException, IOException {
 	  
 	  
-	  String host     = "smtp.naver.com";
-	  final String user   = "statandnow";
+	  String host     = "smtp.gmail.com";
+	  final String user   = "bit.tripadvisor";
 	  final String password  = "vmfhwprxm";
 	  
 	  String email = request.getParameter("email");
@@ -39,8 +39,16 @@ public class EmailController extends HttpServlet {
 	  
 	  // Get the session object
 	  Properties props = new Properties();
+	  props.put("mail.smtp.port", "25");
+	  props.put("mail.debug", "true"); 
 	  props.put("mail.smtp.host", host);
 	  props.put("mail.smtp.auth", "true");
+	  props.put("mail.smtp.starttls.enable","true"); 
+      props.put("mail.smtp.EnableSSL.enable","true");
+      props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");   
+      props.setProperty("mail.smtp.socketFactory.fallback", "false");   
+      props.setProperty("mail.smtp.port", "465");   
+      props.setProperty("mail.smtp.socketFactory.port", "465"); 
 	  
 	  Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 		  protected PasswordAuthentication getPasswordAuthentication() {
